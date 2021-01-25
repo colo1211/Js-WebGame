13f2f4e2893aa143f0b70eb4e64ef8399bb73191
colo1211 <colo1211@naver.com>
2021-01-25 18:15:22 +0900

    ch 8 반응속도 핵심정리

--- ch8. 반응속도테스트/반응속도.js
+++ ch8. 반응속도테스트/반응속도.js
@@ -2,7 +2,7 @@ var 스크린 = document.querySelector('#screen');
 var 시작시간; // undefined
 var 끝시간;
 var 기록 = [];
-var 대기 ;
+var 대기;
 스크린.addEventListener('click',function(){
     if (스크린.classList.contains('waiting')){ // 클래스이므로 #을 뺀다. 현재는 파랑
         스크린.classList.remove('waiting');
@@ -14,12 +14,12 @@ var 대기 ;
             스크린.click();
         },Math.floor(Math.random()*1000)+2000); // 0 ~ 1000 사이의 수
     }else if (스크린.classList.contains('ready')){
-        if (!시작시간){ // 부정 클릭, 빨강색일 때 클릭 | !시작시간 -> 시작시간이 undefined, Null, NaN, False등이 아닐 때
-            clearTimeout(대기);
+        if (!시작시간){ // 부정 클릭 총 쏘기 전에 출발, 빨강색일 때 클릭 | !시작시간 -> 시작시간이 undefined, Null, NaN, False등이 아닐 때
+            clearTimeout(대기); // Timeout 취소.
             스크린.classList.remove('ready');
             스크린.classList.add('waiting'); // 파란색
             스크린.textContent='파란색에서 클릭하지 마세요. 초록색화면으로 넘어가기 기다리세요';
-        }else { // 제대로 기다릴 시, 녹색으로 자동 변경
+        }else { // 정상 출발, 시작시간이 있다면, 일정시간 대기 후 변경
             스크린.classList.remove('ready');
             스크린.classList.add('now'); // 초록색
             스크린.textContent = '클릭!';
@@ -28,7 +28,7 @@ var 대기 ;
         끝시간 = new Date();
         var 반응시간 = (끝시간 - 시작시간)/1000;
         기록.push(반응시간);
-        console.log(반응시간,'초');
+        document.querySelector('#result').textContent=반응시간+'초 걸렸습니다!';
         시작시간 = null;
         끝시간 = null; // 초기화를 해줘야 두번째 클릭에서도 부정행위를 방지 할 수 있다.
         스크린.classList.remove('now');
