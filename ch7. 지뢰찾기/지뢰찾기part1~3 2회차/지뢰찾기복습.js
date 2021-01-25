c0bae44bba3737282c03f7817ee567b8347a5b5d
colo1211 <colo1211@naver.com>
2021-01-25 12:58:28 +0900

    최종 완성 본

--- ch7. 지뢰찾기/지뢰찾기part1~3 2회차/지뢰찾기복습.js
+++ ch7. 지뢰찾기/지뢰찾기part1~3 2회차/지뢰찾기복습.js
@@ -3,15 +3,15 @@ var tbody = document.querySelector('#table tbody');
 var 중단플래그 = false; // 게임 실패를 알리기 위해
 var 열은칸=0; // 게임 성공을 알리기 위해
 
-// var 코드표 ={
-//     연칸 : -1,
-//     물음표 : -2,
-//     깃발 : -3,
-//     깃발지뢰 :-4,
-//     물음표지뢰:-5,
-//     지뢰 :1,
-//     보통칸: 0 ,
-// };
+var 코드표 ={
+    연칸 : -1,
+    물음표 : -2,
+    깃발 : -3,
+    깃발지뢰 :-4,
+    물음표지뢰:-5,
+    지뢰 :1,
+    보통칸: 0 ,
+};
 
 document.querySelector('#btn').addEventListener('click',function(){
     tbody.innerHTML=''; // tbody의 내부 태그들을 모두 삭제
@@ -19,15 +19,15 @@ document.querySelector('#btn').addEventListener('click',function(){
     document.querySelector('#result').textContent=' ';
     열은칸=0;
     중단플래그=false;
-    var 가로= parseInt(document.querySelector('#hor').value);
-    var 세로= parseInt(document.querySelector('#ver').value);
-    var 지뢰= parseInt(document.querySelector('#mine').value);
+    var hor= parseInt(document.querySelector('#hor').value);
+    var ver= parseInt(document.querySelector('#ver').value);
+    var mine= parseInt(document.querySelector('#mine').value);
     // console.log(가로,세로,지뢰);
 
     // 지뢰의 위치를 랜덤하게 뽑아준다.
-    var 후보군 = Array(가로*세로).fill().map(function (element,index){return index})
+    var 후보군 = Array(hor*ver).fill().map(function (element,index){return index})
     var 셔플 =[];
-    while(후보군.length>(100-지뢰)){
+    while(후보군.length>(hor*ver-mine)){
         var temp = 후보군.splice(Math.floor(Math.random()*후보군.length),1)[0];
         셔플.push(temp);
     }
@@ -36,14 +36,14 @@ document.querySelector('#btn').addEventListener('click',function(){
 
 
     // tbody 내에 테이블 생성
-    for (var i=0; i<세로; i++){
+    for (var i=0; i<ver; i++){
         var tr = document.createElement('tr');
         var arr= [];
         dataset.push(arr);
-        for (var j=0;j<가로;j++){
+        for (var j=0;j<hor;j++){
             var td = document.createElement('td');
 
-            arr.push(0);
+            arr.push(코드표.보통칸);
             //우클릭
             td.addEventListener('contextmenu',function (e){
                 if (중단플래그 === true) return ;
@@ -55,13 +55,24 @@ document.querySelector('#btn').addEventListener('click',function(){
 
                 if (e.currentTarget.textContent==='' || e.currentTarget.textContent==='X'){
                     e.currentTarget.textContent='!';
+                    e.currentTarget.classList.toggle('flag');
+                    if (dataset[줄][칸]=== 코드표.지뢰) dataset[줄][칸]=코드표.깃발지뢰;
+                    else dataset[줄][칸]= 코드표.깃발;
                 }else if (e.currentTarget.textContent==='!'){
                     e.currentTarget.textContent='?';
+                    e.currentTarget.classList.toggle('flag');
+                    e.currentTarget.classList.toggle('question');
+                    if (dataset[줄][칸]=== 코드표.깃발지뢰) dataset[줄][칸]=코드표.물음표지뢰;
+                    else dataset[줄][칸]= 코드표.물음표;
                 }else if (e.currentTarget.textContent==='?'){
-                    if (dataset[줄][칸]==='X'){
+                    e.currentTarget.classList.toggle('question');
+
+                    if (dataset[줄][칸]===코드표.지뢰 || dataset[줄][칸]===코드표.깃발지뢰 || dataset[줄][칸]===코드표.물음표지뢰){
                         e.currentTarget.textContent='X';
+                        dataset[줄][칸]=코드표.지뢰;
                     }else{
                         e.currentTarget.textContent=' ';
+                        dataset[줄][칸]=코드표.보통칸;
                     }
                 }
             })
@@ -74,19 +85,21 @@ document.querySelector('#btn').addEventListener('click',function(){
                 var 줄 = Array.prototype.indexOf.call(부모tbody.children,부모tr);
                 var 칸 = Array.prototype.indexOf.call(부모tr.children,e.currentTarget);
 
+                if (dataset[줄][칸]===코드표.연칸 || dataset[줄][칸]===코드표.물음표지뢰|| dataset[줄][칸]===코드표.깃발지뢰 || dataset[줄][칸]===코드표.물음표||
+                dataset[줄][칸]===코드표.깃발) return;
+
                 // 동일한 칸을 클릭했을때, 카운트 안되게 막아줌
-                if (dataset[줄][칸]===1) return;
 
                 //열었을 때, 하얀색 배경
                 e.currentTarget.classList.add('open');
                 열은칸+=1;
 
-                if (dataset[줄][칸]==='X'){ // 클릭했을때 지뢰일때
+                if (dataset[줄][칸]===코드표.지뢰){ // 클릭했을때 지뢰일때
                     e.currentTarget.textContent='펑';
                     document.querySelector('#result').textContent='실패';
                     중단플래그 = true; // 게임 종료
                 }else { // 클릭했을때 지뢰 아닐 때, 주변의 지뢰 개수를 카운트
-                    dataset[줄][칸]=1; // 열은 칸을 1로 변경
+                    dataset[줄][칸]=코드표.연칸; // 열은 칸을 1로 변경
                     var 주변지뢰 = [dataset[줄][칸-1],dataset[줄][칸+1]];
                     if (dataset[줄-1]){ // 줄-1이 undefined가 아닐 때, 칸이 있을 때 합체!
                         주변지뢰 = 주변지뢰.concat(dataset[줄-1][칸-1],dataset[줄-1][칸],dataset[줄-1][칸+1]);
@@ -95,7 +108,7 @@ document.querySelector('#btn').addEventListener('click',function(){
                         주변지뢰= 주변지뢰.concat(dataset[줄+1][칸-1],dataset[줄+1][칸],dataset[줄+1][칸+1]);
                     }
                     var 주변지뢰개수 =주변지뢰.filter(function(element){
-                        return element === 'X';
+                        return [코드표.지뢰,코드표.물음표지뢰,코드표.깃발지뢰].includes(element)
                     }).length;
                     e.currentTarget.textContent=주변지뢰개수 || '';
                     // 거짓인 값들('',0,NaN,null,undefined,false)을 ''로 대신 출력하라는 의미
@@ -126,7 +139,7 @@ document.querySelector('#btn').addEventListener('click',function(){
                             var 부모tbody= v.parentNode.parentNode;
                             var 옆칸줄 = Array.prototype.indexOf.call(부모tbody.children,부모tr);
                             var 옆칸칸 = Array.prototype.indexOf.call(부모tr.children,v);
-                            if (dataset[옆칸줄][옆칸칸]!==1){
+                            if (dataset[옆칸줄][옆칸칸]!==코드표.연칸){
                                 v.click(); // 재귀
                             }
                             });
@@ -151,10 +164,10 @@ document.querySelector('#btn').addEventListener('click',function(){
     }
 
     for (var x=0;x<셔플.length;x++){
-        var 세로= Math.floor(셔플[x]/10);
-        var 가로= (셔플[x]%10);
+        var 세로= Math.floor(셔플[x] / ver);
+        var 가로= (셔플[x] % ver);
         tbody.children[세로].children[가로].textContent= 'X'; // 화면
-        dataset[세로][가로]='X'; // 데이타셋
+        dataset[세로][가로]=코드표.지뢰; // 데이타셋
     }
 
 });
